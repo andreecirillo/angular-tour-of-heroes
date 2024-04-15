2583a1d763c9e1c96d2dfe944de08234876559e1
André Cirillo <andreecirillo@gmail.com>
2024-04-15 18:56:04 -0300

    displaying a list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { UpperCasePipe, NgFor, NgIf } from '@angular/common';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule
+  imports: [    
+    FormsModule,
+    UpperCasePipe,
+    NgFor,
+    NgIf
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
